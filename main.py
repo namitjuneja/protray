947552d0b24ef64247075f740381493574e81c5c
Your Name <juneja.namit@gmail.com>
2016-01-23 15:46:29 -0500

    added the weather parameter

--- main.py
+++ main.py
@@ -20,8 +20,13 @@ def location():
 #template creation route
 @app.route('/1/weather')
 def location():
-    add_location_record(request)
-    add_weather_update(request)
+    weather_data = get_weather_record(request) #returns python dict with key as day and value another dict {"location" : location, "description" : description, "temp" : temp, "humidity" : humidity}
+    for day in weather_data.keys():
+    	if weather_data[day][temperature] < 5:
+    		if weather_data[day][wind] < 
+    	elif weather_data[day][temperature] >= 5 and weather_data[day][temperature] <= 30:
+    	else:
+
     return json.dumps(data)
 
 if __name__ == '__main__':
