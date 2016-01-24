b5d30250762fda6b74ad19ae97f8c4b2fc746181
Ankesh Anand <ankeshanand1994@gmail.com>
2016-01-23 22:16:43 -0500

    Tabs instead of spaces.

--- location_update.py
+++ location_update.py
@@ -5,20 +5,18 @@ db = MySQLdb.connect("sql6.freemysqlhosting.net","sql6103440","bqz3y1t3aU","sql6
 
 
 def add_location_record(request):
-	details = {"timestamp" : request.form["timestamp"], "lat" : request.form["lat"], "lon" : request.form["lon"]}
-	cursor = db.cursor() # prepare a cursor object using cursor() method
-	sql = """INSERT INTO user_data (name, date, month, year, start_time, end_time, destination, contact, comments) 
-		VALUES (%s, %s,%s, %s, %s, %s, %s, %s, %s);""", (str(request.form["name"]), str(request.form["date"]), str(request.form["month"]), str(request.form["year"]), str(request.form["start_time"]), str(request.form["end_time"]), str(request.form["destination"]), str(request.form["number"]), str(request.form["comments"]))
-	cursor.execute(sql) # execute SQL query using execute() method.
-	db.commit()
-	db.close()  # disconnect from server
+    details = {"timestamp" : request.form["timestamp"], "lat" : request.form["lat"], "lon" : request.form["lon"]}
+    cursor = db.cursor() # prepare a cursor object using cursor() method
+    sql = """INSERT INTO user_data (name, date, month, year, start_time, end_time, destination, contact, comments) VALUES (%s, %s,%s, %s, %s, %s, %s, %s, %s);""", (str(request.form["name"]), str(request.form["date"]), str(request.form["month"]), str(request.form["year"]), str(request.form["start_time"]), str(request.form["end_time"]), str(request.form["destination"]), str(request.form["number"]), str(request.form["comments"]))
+    cursor.execute(sql) # execute SQL query using execute() method.
+    db.commit()
+    db.close()  # disconnect from server
 
-def add_location_record(request):
-	details = {"timestamp" : request.form["timestamp"], "lat" : request.form["lat"], "lon" : request.form["lon"]}
-	
-	cursor = db.cursor() # prepare a cursor object using cursor() method
-	sql = """INSERT INTO user_data (name, date, month, year, start_time, end_time, destination, contact, comments) 
-		VALUES (%s, %s,%s, %s, %s, %s, %s, %s, %s);""", (str(request.form["name"]), str(request.form["date"]), str(request.form["month"]), str(request.form["year"]), str(request.form["start_time"]), str(request.form["end_time"]), str(request.form["destination"]), str(request.form["number"]), str(request.form["comments"]))
-	cursor.execute(sql) # execute SQL query using execute() method.
-	db.commit()
-	db.close()  # disconnect from server
+def add_weather_record(request):
+    details = {"timestamp" : request.form["timestamp"], "lat" : request.form["lat"], "lon" : request.form["lon"]}
+    cursor = db.cursor() # prepare a cursor object using cursor() method
+    sql = """INSERT INTO user_data (name, date, month, year, start_time, end_time, destination, contact, comments) 
+    VALUES (%s, %s,%s, %s, %s, %s, %s, %s, %s);""", (str(request.form["name"]), str(request.form["date"]), str(request.form["month"]), str(request.form["year"]), str(request.form["start_time"]), str(request.form["end_time"]), str(request.form["destination"]), str(request.form["number"]), str(request.form["comments"]))
+    cursor.execute(sql) # execute SQL query using execute() method.
+    db.commit()
+    db.close()  # disconnect from server
